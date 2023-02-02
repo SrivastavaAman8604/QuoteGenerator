088ef0245bf7306e7a2a797085c1e47a915e70a2
Aman Srivastava <aman.srivastava2639@gmail.com>
2023-02-02 15:14:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const[quote,setQuote] = useState("Demo Quote");
+
+  const handlequote = async () => {
+    const res  = await axios.get('https://api.quotable.io/random');
+    // console.log(data);
+    setQuote(res.data.content);
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div  className="box">
+          <h3>Quote of the Day</h3>
+          <p>{quote}</p>
+          <button style={{cursor:"pointer"}} onClick={handlequote}>Next</button>
+        </div>
       </header>
     </div>
   );
